9cd2857a545582bdaa1a00d22d618e6248b6303d
Alejandro <alexelninja@gmail.com>
2022-01-11 13:18:06 +0100

    Added working openstreetmaps

--- lib/screens/osm_screen.dart
+++ lib/screens/osm_screen.dart
@@ -1,17 +1,599 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:absurd_toolbox/widgets/_general/layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
-class OSMScreen extends StatelessWidget {
+class CustomController extends MapController {
+  CustomController({
+    bool initMapWithUserPosition = true,
+    GeoPoint? initPosition,
+    BoundingBox? areaLimit = const BoundingBox.world(),
+  })  : assert(
+          initMapWithUserPosition || initPosition != null,
+        ),
+        super(
+          initMapWithUserPosition: initMapWithUserPosition,
+          initPosition: initPosition,
+          areaLimit: areaLimit,
+        );
+
+  @override
+  void init() {
+    super.init();
+  }
+}
+
+class OSMScreen extends StatefulWidget {
   static const String routeName = '/osmaps';
+
+  @override
+  _OSMScreenState createState() => _OSMScreenState();
+}
+
+class _OSMScreenState extends State<OSMScreen> with OSMMixinObserver {
+  late CustomController controller;
+  late GlobalKey<ScaffoldState> scaffoldKey;
+  Key mapGlobalkey = UniqueKey();
+  ValueNotifier<bool> zoomNotifierActivation = ValueNotifier(false);
+  ValueNotifier<bool> visibilityZoomNotifierActivation = ValueNotifier(false);
+  ValueNotifier<bool> advPickerNotifierActivation = ValueNotifier(false);
+  ValueNotifier<bool> trackingNotifier = ValueNotifier(false);
+  ValueNotifier<bool> showFab = ValueNotifier(true);
+  ValueNotifier<GeoPoint?> lastGeoPoint = ValueNotifier(null);
+  Timer? timer;
+  int x = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CustomController(
+      initMapWithUserPosition: false,
+      initPosition: GeoPoint(
+        latitude: 47.4358055,
+        longitude: 8.4737324,
+      ),
+      // areaLimit: BoundingBox(
+      //   east: 10.4922941,
+      //   north: 47.8084648,
+      //   south: 45.817995,
+      //   west: 5.9559113,
+      // ),
+    );
+    controller.addObserver(this);
+    scaffoldKey = GlobalKey<ScaffoldState>();
+    controller.listenerMapLongTapping.addListener(() async {
+      if (controller.listenerMapLongTapping.value != null) {
+        print(controller.listenerMapLongTapping.value);
+        await controller.addMarker(
+          controller.listenerMapLongTapping.value!,
+          markerIcon: MarkerIcon(
+            icon: Icon(
+              Icons.store,
+              color: Colors.brown,
+              size: 48,
+            ),
+          ),
+          angle: pi / 3,
+        );
+      }
+    });
+    controller.listenerMapSingleTapping.addListener(() async {
+      if (controller.listenerMapSingleTapping.value != null) {
+        if (lastGeoPoint.value != null) {
+          controller.removeMarker(lastGeoPoint.value!);
+        }
+        print(controller.listenerMapSingleTapping.value);
+        lastGeoPoint.value = controller.listenerMapSingleTapping.value;
+        await controller.addMarker(
+          lastGeoPoint.value!,
+          markerIcon: MarkerIcon(
+            // icon: Icon(
+            //   Icons.person_pin,
+            //   color: Colors.red,
+            //   size: 32,
+            // ),
+            image: AssetImage("asset/pin.png"),
+            // assetMarker: AssetMarker(
+            //   image: AssetImage("asset/pin.png"),
+            //   //scaleAssetImage: 2,
+            // ),
+          ),
+          //angle: -pi / 4,
+        );
+      }
+    });
+    controller.listenerRegionIsChanging.addListener(() async {
+      if (controller.listenerRegionIsChanging.value != null) {
+        print(controller.listenerRegionIsChanging.value);
+      }
+    });
+
+    //controller.listenerMapIsReady.addListener(mapIsInitialized);
+  }
+
+  Future<void> mapIsInitialized() async {
+    //await controller.setZoom(zoomLevel: 12);
+    // await controller.setMarkerOfStaticPoint(
+    //   id: "line 1",
+    //   markerIcon: MarkerIcon(
+    //     icon: Icon(
+    //       Icons.train,
+    //       color: Colors.red,
+    //       size: 48,
+    //     ),
+    //   ),
+    // );
+    await controller.setMarkerOfStaticPoint(
+      id: "line 2",
+      markerIcon: MarkerIcon(
+        icon: Icon(
+          Icons.train,
+          color: Colors.orange,
+          size: 48,
+        ),
+      ),
+    );
+
+    await controller.setStaticPosition(
+      [
+        GeoPointWithOrientation(
+          latitude: 47.4433594,
+          longitude: 8.4680184,
+          angle: pi / 4,
+        ),
+        GeoPointWithOrientation(
+          latitude: 47.4517782,
+          longitude: 8.4716146,
+          angle: pi / 2,
+        ),
+      ],
+      "line 2",
+    );
+    final bounds = await controller.bounds;
+    print(bounds.toString());
+  }
+
+  @override
+  Future<void> mapIsReady(bool isReady) async {
+    if (isReady) {
+      await mapIsInitialized();
+    }
+  }
+
+  @override
+  void dispose() {
+    if (timer != null && timer!.isActive) {
+      timer?.cancel();
+    }
+    //controller.listenerMapIsReady.removeListener(mapIsInitialized);
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Layout(
-      statusBarColor: Colors.orange.shade600,
-      title: "Open Street Maps",
-      themeColor: Colors.orange,
+      statusBarColor: Colors.lightGreen.shade600,
+      title: "Google Maps",
+      themeColor: Colors.lightGreen,
       showAppBar: true,
-      content: Center(),
+      // key: scaffoldKey,
+      // resizeToAvoidBottomInset: false,
+      // appBar: AppBar(
+      //   title: const Text('OSM'),
+      //   leading: ValueListenableBuilder<bool>(
+      //     valueListenable: advPickerNotifierActivation,
+      //     builder: (ctx, isAdvancedPicker, _) {
+      //       if (isAdvancedPicker) {
+      //         return IconButton(
+      //           onPressed: () {
+      //             advPickerNotifierActivation.value = false;
+      //             controller.cancelAdvancedPositionPicker();
+      //           },
+      //           icon: Icon(Icons.close),
+      //         );
+      //       }
+      //       return SizedBox.shrink();
+      //     },
+      //   ),
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: Icon(Icons.info),
+      //       onPressed: () async {
+      //         await Navigator.popAndPushNamed(context, "/second");
+      //       },
+      //     ),
+      //     Builder(builder: (ctx) {
+      //       return IconButton(
+      //         onPressed: () => roadActionBt(ctx),
+      //         icon: Icon(Icons.map),
+      //       );
+      //     }),
+      //     IconButton(
+      //       onPressed: () async {
+      //         visibilityZoomNotifierActivation.value = !visibilityZoomNotifierActivation.value;
+      //         zoomNotifierActivation.value = !zoomNotifierActivation.value;
+      //       },
+      //       icon: Icon(Icons.zoom_out_map),
+      //     ),
+      //     IconButton(
+      //       onPressed: () async {
+      //         await Navigator.pushNamed(context, "/picker-result");
+      //       },
+      //       icon: Icon(Icons.search),
+      //     ),
+      //     IconButton(
+      //       icon: Icon(Icons.select_all),
+      //       onPressed: () async {
+      //         if (advPickerNotifierActivation.value == false) {
+      //           advPickerNotifierActivation.value = true;
+      //           await controller.advancedPositionPicker();
+      //         }
+      //       },
+      //     )
+      //   ],
+      // ),
+      content: Container(
+        child: Stack(
+          children: [
+            OSMFlutter(
+              controller: controller,
+              androidHotReloadSupport: true,
+              mapIsLoading: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    Text("Map is Loading..")
+                  ],
+                ),
+              ),
+              initZoom: 8,
+              minZoomLevel: 8,
+              maxZoomLevel: 14,
+              stepZoom: 1.0,
+              userLocationMarker: UserLocationMaker(
+                personMarker: MarkerIcon(
+                  icon: Icon(
+                    Icons.location_history_rounded,
+                    color: Colors.red,
+                    size: 48,
+                  ),
+                ),
+                directionArrowMarker: MarkerIcon(
+                  icon: Icon(
+                    Icons.double_arrow,
+                    size: 48,
+                  ),
+                ),
+              ),
+              showContributorBadgeForOSM: true,
+              //trackMyPosition: trackingNotifier.value,
+              showDefaultInfoWindow: false,
+              onLocationChanged: (myLocation) {
+                print(myLocation);
+              },
+              onGeoPointClicked: (geoPoint) async {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "${geoPoint.toMap().toString()}",
+                    ),
+                    action: SnackBarAction(
+                      onPressed: () =>
+                          ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+                      label: "hide",
+                    ),
+                  ),
+                );
+              },
+              staticPoints: [
+                StaticPositionGeoPoint(
+                  "line 1",
+                  MarkerIcon(
+                    icon: Icon(
+                      Icons.train,
+                      color: Colors.green,
+                      size: 48,
+                    ),
+                  ),
+                  [
+                    GeoPoint(latitude: 47.4333594, longitude: 8.4680184),
+                    GeoPoint(latitude: 47.4317782, longitude: 8.4716146),
+                  ],
+                ),
+                /*StaticPositionGeoPoint(
+                      "line 2",
+                      MarkerIcon(
+                        icon: Icon(
+                          Icons.train,
+                          color: Colors.red,
+                          size: 48,
+                        ),
+                      ),
+                      [
+                        GeoPoint(latitude: 47.4433594, longitude: 8.4680184),
+                        GeoPoint(latitude: 47.4517782, longitude: 8.4716146),
+                      ],
+                    )*/
+              ],
+              roadConfiguration: RoadConfiguration(
+                startIcon: MarkerIcon(
+                  icon: Icon(
+                    Icons.person,
+                    size: 64,
+                    color: Colors.brown,
+                  ),
+                ),
+                roadColor: Colors.red,
+              ),
+              markerOption: MarkerOption(
+                defaultMarker: MarkerIcon(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.orange,
+                    size: 64,
+                  ),
+                ),
+                advancedPickerMarker: MarkerIcon(
+                  icon: Icon(
+                    Icons.location_searching,
+                    color: Colors.green,
+                    size: 64,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              left: 10,
+              child: ValueListenableBuilder<bool>(
+                valueListenable: advPickerNotifierActivation,
+                builder: (ctx, visible, child) {
+                  return Visibility(
+                    visible: visible,
+                    child: AnimatedOpacity(
+                      opacity: visible ? 1.0 : 0.0,
+                      duration: Duration(milliseconds: 500),
+                      child: child,
+                    ),
+                  );
+                },
+                child: FloatingActionButton(
+                  key: UniqueKey(),
+                  child: Icon(Icons.arrow_forward),
+                  heroTag: "confirmAdvPicker",
+                  onPressed: () async {
+                    advPickerNotifierActivation.value = false;
+                    GeoPoint p =
+                        await controller.selectAdvancedPositionPicker();
+                    print(p);
+                  },
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              left: 10,
+              child: ValueListenableBuilder<bool>(
+                valueListenable: visibilityZoomNotifierActivation,
+                builder: (ctx, visibility, child) {
+                  return Visibility(
+                    visible: visibility,
+                    child: child!,
+                  );
+                },
+                child: ValueListenableBuilder<bool>(
+                  valueListenable: zoomNotifierActivation,
+                  builder: (ctx, isVisible, child) {
+                    return AnimatedOpacity(
+                      opacity: isVisible ? 1.0 : 0.0,
+                      onEnd: () {
+                        visibilityZoomNotifierActivation.value = isVisible;
+                      },
+                      duration: Duration(milliseconds: 500),
+                      child: child,
+                    );
+                  },
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        child: Icon(Icons.add),
+                        onPressed: () async {
+                          controller.zoomIn();
+                        },
+                      ),
+                      ElevatedButton(
+                        child: Icon(Icons.remove),
+                        onPressed: () async {
+                          controller.zoomOut();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      fab: ValueListenableBuilder<bool>(
+        valueListenable: showFab,
+        builder: (ctx, isShow, child) {
+          if (!isShow) {
+            return SizedBox.shrink();
+          }
+          return child!;
+        },
+        child: FloatingActionButton(
+          onPressed: () async {
+            if (!trackingNotifier.value) {
+              await controller.currentLocation();
+              await controller.enableTracking();
+              //await controller.zoom(5.0);
+            } else {
+              await controller.disabledTracking();
+            }
+            trackingNotifier.value = !trackingNotifier.value;
+          },
+          child: ValueListenableBuilder<bool>(
+            valueListenable: trackingNotifier,
+            builder: (ctx, isTracking, _) {
+              if (isTracking) {
+                return Icon(Icons.gps_off_sharp);
+              }
+              return Icon(Icons.my_location);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  void roadActionBt(BuildContext ctx) async {
+    try {
+      await controller.removeLastRoad();
+
+      ///selection geoPoint
+      GeoPoint point = await controller.selectPosition(
+          icon: MarkerIcon(
+        icon: Icon(
+          Icons.person_pin_circle,
+          color: Colors.amber,
+          size: 100,
+        ),
+      ));
+      GeoPoint point2 = await controller.selectPosition();
+      showFab.value = false;
+      ValueNotifier<RoadType> notifierRoadType = ValueNotifier(RoadType.car);
+
+      final bottomPersistant = scaffoldKey.currentState!.showBottomSheet(
+        (ctx) {
+          return RoadTypeChoiceWidget(
+            setValueCallback: (roadType) {
+              notifierRoadType.value = roadType;
+            },
+          );
+        },
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      );
+      await bottomPersistant.closed.then((roadType) async {
+        showFab.value = true;
+        RoadInfo roadInformation = await controller.drawRoad(
+          point, point2,
+          roadType: notifierRoadType.value,
+          //interestPoints: [pointM1, pointM2],
+          roadOption: RoadOption(
+              roadWidth: 10,
+              roadColor: Colors.blue,
+              showMarkerOfPOI: false,
+              zoomInto: true),
+        );
+        print(
+            "duration:${Duration(seconds: roadInformation.duration!.toInt()).inMinutes}");
+        print("distance:${roadInformation.distance}Km");
+        print(roadInformation.route.length);
+        // final box = await BoundingBox.fromGeoPointsAsync([point2, point]);
+        // controller.zoomToBoundingBox(
+        //   box,
+        //   paddinInPixel: 64,
+        // );
+      });
+    } on RoadException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "${e.errorMessage()}",
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<void> mapRestored() async {
+    super.mapRestored();
+    print("log map restored");
+  }
+}
+
+class RoadTypeChoiceWidget extends StatelessWidget {
+  final Function(RoadType road) setValueCallback;
+
+  RoadTypeChoiceWidget({
+    required this.setValueCallback,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 96,
+      child: WillPopScope(
+        onWillPop: () async => false,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 64,
+            width: 196,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    // setValueCallback(RoadType.car);
+                    // Navigator.pop(context, RoadType.car);
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.directions_car),
+                      Text("Car"),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // setValueCallback(RoadType.bike);
+                    // Navigator.pop(context);
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.directions_bike),
+                      Text("Bike"),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // setValueCallback(RoadType.foot);
+                    // Navigator.pop(context);
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Icon(Icons.directions_walk),
+                      Text("Foot"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
